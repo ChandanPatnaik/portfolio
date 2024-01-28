7bc8c1279e9f81a351d52e8f8e27adf918927ef6
Chandan Kumar Patnaik <chandanpatnaik81@gmail.com>
2024-01-28 10:09:17 +0530

    about section complete

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -26,7 +26,38 @@ const TimeLine = () => {
 
 const AboutBlock = () => {
   return (
-    <section className="flex flex-col">
+    <section className="flex flex-col relative">
+      <div className="w-fit h-full absolute top-0 -left-16">
+        <div className="flex flex-col text-milk/30 tracking-wide leading-6 text-sm">
+          <p>01</p>
+          <p>02</p>
+          <p>03</p>
+          <p>04</p>
+          <p>05</p>
+          <p>06</p>
+          <p>07</p>
+          <p>08</p>
+          <p>09</p>
+          <p>10</p>
+          <p>11</p>
+          <p>12</p>
+          <p>13</p>
+          <p>14</p>
+          <p>15</p>
+          <p>16</p>
+          <p className="opacity-0">17</p>
+          <p>17</p>
+          <p>18</p>
+          <p>19</p>
+          <p>20</p>
+          <p>21</p>
+          <p>22</p>
+          <p>23</p>
+          <p>24</p>
+          <p>25</p>
+        </div>
+      </div>
+
       {/* --------------- class section ------------ */}
       <div className="flex items-center gap-2 tracking-wide">
         <p className="text-red-600">export</p>
@@ -54,81 +85,169 @@ const AboutBlock = () => {
       </div>
 
       {/* --------------- constructor section ------------ */}
-      <div className="flex px-4 items-center tracking-wide gap-2">
-        <div className="flex items-center">
-          <p className="text-red-600">{`constructor`}</p>
-          <p className="text-milk font-normal">{`( )`}</p>
+      <div className="px-4 flex flex-col">
+        <div className="flex items-center tracking-wide gap-2">
+          <div className="flex items-center">
+            <p className="text-red-600">{`constructor`}</p>
+            <p className="text-milk font-normal">{`( )`}</p>
+          </div>
+          <p className="text-milk">{`{`}</p>
         </div>
-        <p className="text-milk">{`{`}</p>
-      </div>
 
-      {/* --------------- constructor inside section ------------ */}
-      <div className=" px-8 flex flex-col">
-        <div className="flex items-center gap-2">
-          <div className="flex items-center">
-            <p className="text-blue-400">this.</p>
-            <p className="text-white">name</p>
+        {/* --------------- constructor inside section ------------ */}
+        <div className="px-4 flex flex-col">
+          <div className="flex items-center gap-2">
+            <div className="flex items-center">
+              <p className="text-blue-400">this.</p>
+              <p className="text-white">name</p>
+            </div>
+            <div className="text-red-400">{`=`}</div>
+            <div className="text-blue-300">{`'Chandan Kumar Patnaik'`}</div>
           </div>
-          <div className="text-red-400">{`=`}</div>
-          <div className="text-blue-300">{`'Chandan Kumar Patnaik'`}</div>
-        </div>
-        <div className="flex items-center gap-2">
-          <div className="flex items-center">
-            <p className="text-blue-400">this.</p>
-            <p className="text-white">dayOfBirthTimestamp</p>
+          <div className="flex items-center gap-2">
+            <div className="flex items-center">
+              <p className="text-blue-400">this.</p>
+              <p className="text-white">dayOfBirthTimestamp</p>
+            </div>
+            <div className="text-red-400">{`=`}</div>
+            <div className="text-blue-300">{`948047400`}</div>
           </div>
-          <div className="text-red-400">{`=`}</div>
-          <div className="text-blue-300">{`948047400`}</div>
-        </div>
-        <div className="flex items-center gap-2">
-          <div className="flex items-center">
-            <p className="text-blue-400">this.</p>
-            <p className="text-white">email</p>
+          <div className="flex items-center gap-2">
+            <div className="flex items-center">
+              <p className="text-blue-400">this.</p>
+              <p className="text-white">email</p>
+            </div>
+            <div className="text-red-400">{`=`}</div>
+            <div className="text-blue-300">{`'chandanpatnaik81@gmail.com'`}</div>
           </div>
-          <div className="text-red-400">{`=`}</div>
-          <div className="text-blue-300">{`'chandanpatnaik81@gmail.com'`}</div>
         </div>
-      </div>
 
-      {/* ------------ bracket close of constructor */}
-      <div className=" px-4 text-milk">{`}`}</div>
+        <div className="text-milk">{`}`}</div>
+      </div>
 
       {/* --------------- work method section ------------ */}
-      <div className="flex px-4 items-center tracking-wide gap-2">
-        <div className="flex items-center">
-          <p className="text-light-yellow">{`workExperience`}</p>
-          <p className="text-milk font-normal">{`( )`}</p>
+      <div className="px-4 flex flex-col">
+        <div className="flex items-center tracking-wide gap-2">
+          <div className="flex items-center">
+            <p className="text-light-yellow">{`workExperience`}</p>
+            <p className="text-milk font-normal">{`( )`}</p>
+          </div>
+          <p className="text-milk">{`{`}</p>
         </div>
-        <p className="text-milk">{`{`}</p>
+
+        <div className=" px-4 flex flex-col">
+          <div className="flex items-center gap-2">
+            <p className="text-red-500">{`return`}</p>
+            <p className="text-milk">{`[`}</p>
+          </div>
+          <div className="flex flex-col px-4">
+            <div className="flex items-center gap-2">
+              <p className="text-milk">{`{`}</p>
+              <p className="text-green-400">{`'Jun 2022-now'`}</p>
+              <p className="text-green-400">{`:`}</p>
+              <p className="text-green-400">{`'Software Developer at SearchingYard Group'`}</p>
+              <p className="text-milk">{`},`}</p>
+            </div>
+            <div className="flex items-center gap-2">
+              <p className="text-milk">{`{`}</p>
+              <p className="text-green-400">{`'Jun 2022-Nov 2022'`}</p>
+              <p className="text-green-400">{`:`}</p>
+              <p className="text-green-400">{`'Software Intern at SearchingYard Group'`}</p>
+              <p className="text-milk">{`},`}</p>
+            </div>
+          </div>
+          <p className="text-milk">{`]`}</p>
+        </div>
+
+        <p className="text-milk">{`}`}</p>
       </div>
 
-      {/* --------------- work method inside section ------------ */}
-      <div className=" px-8 flex flex-col">
-        <div className="flex items-center gap-2">
-          <p className="text-red-500">{`return`}</p>
-          <p className="text-milk">{`[`}</p>
-        </div>
-        <div className="flex flex-col px-4">
-          <div className="flex items-center gap-2">
-            <p className="text-milk">{`{`}</p>
-            <p className="text-green-400">{`'Jun 2022-now'`}</p>
-            <p className="text-green-400">{`:`}</p>
-            <p className="text-green-400">{`'Software Developer at SearchingYard Group'`}</p>
-            <p className="text-milk">{`},`}</p>
+      {/* --------------- education method section ------------ */}
+      <div className="px-4 flex flex-col">
+        <div className="flex items-center tracking-wide gap-2">
+          <div className="flex items-center">
+            <p className="text-light-yellow">{`education`}</p>
+            <p className="text-milk font-normal">{`( )`}</p>
           </div>
-          <div className="flex items-center gap-2">
-            <p className="text-milk">{`{`}</p>
-            <p className="text-green-400">{`'Jun 2022-Nov 2022'`}</p>
-            <p className="text-green-400">{`:`}</p>
-            <p className="text-green-400">{`'Software Intern at SearchingYard Group'`}</p>
-            <p className="text-milk">{`},`}</p>
-          </div>
+          <p className="text-milk">{`{`}</p>
         </div>
-        <p className="text-milk">{`]`}</p>
+
+        <div className=" px-4 flex flex-col">
+          <div className="flex items-center gap-2">
+            <p className="text-red-500">{`return`}</p>
+            <p className="text-milk">{`[`}</p>
+          </div>
+          <div className="flex flex-col pl-4">
+            <div className="flex items-center gap-2">
+              <p className="text-milk">{`{`}</p>
+              <p className="text-green-400">{`'2020-2022'`}</p>
+              <p className="text-green-400">{`:`}</p>
+              <p className="text-green-400">{`'NIIS Institute of Business Administration - Master of Computer `}</p>
+            </div>
+            <div className="flex items-center gap-2">
+              <p className="text-green-400">{`'Science`}</p>
+              <p className="text-milk">{`},`}</p>
+            </div>
+            <div className="flex items-center gap-2">
+              <p className="text-milk">{`{`}</p>
+              <p className="text-green-400">{`'2017-2020'`}</p>
+              <p className="text-green-400">{`:`}</p>
+              <p className="text-green-400">{`'Government College Koraput - Bachelor of Computer Science'`}</p>
+              <p className="text-milk">{`},`}</p>
+            </div>
+          </div>
+          <p className="text-milk">{`]`}</p>
+        </div>
+
+        <p className="text-milk">{`}`}</p>
       </div>
 
-      {/* ---------------end of work method inside section ------------ */}
-      <p className="px-4 text-milk">{`}`}</p>
+      {/* --------------- skills method section ------------ */}
+      <div className="px-4 flex flex-col">
+        <div className="flex items-center tracking-wide gap-2">
+          <div className="flex items-center">
+            <p className="text-light-yellow">{`skills`}</p>
+            <p className="text-milk font-normal">{`( )`}</p>
+          </div>
+          <p className="text-milk">{`{`}</p>
+        </div>
+
+        <div className=" px-4 flex flex-col">
+          <div className="flex items-center gap-2">
+            <p className="text-red-500">{`return`}</p>
+            <p className="text-milk">{`[`}</p>
+            <div className="flex items-center gap-2 flex-wrap text-green-400">
+              <p className="">{`'HTML/CSS/JS',`}</p>
+              <p className="">{`'Tailwind CSS',`}</p>
+              <p className="">{`'Material UI',`}</p>
+              <p className="">{`'React',`}</p>
+              <p className="">{`'Next.js',`}</p>
+              <p className="">{`'Formik'`}</p>
+              <p className="">{`'Yup',`}</p>
+            </div>
+          </div>
+          <div className="flex items-center gap-2 flex-wrap text-green-400">
+            <p className="">{`'Node.js',`}</p>
+            <p className="">{`'Express',`}</p>
+            <p className="">{`'MongoDB',`}</p>
+            <p className="">{`'Mongoose',`}</p>
+            <p className="">{`'Prisma',`}</p>
+            <p className="">{`'npm/yarn'`}</p>
+            <p className="">{`'Typescript',`}</p>
+            <p className="">{`'Redis',`}</p>
+          </div>
+          <div className="flex items-center gap-2 flex-wrap text-green-400">
+            <p className="">{`'S3',`}</p>
+            <p className="">{`'EC2',`}</p>
+            <p className="animate-pulse-custom mx-2 bg-milk w-0.5 h-4"></p>
+            <p className="text-milk">{`]`}</p>
+          </div>
+        </div>
+
+        <p className="text-milk">{`}`}</p>
+      </div>
+
+      <p className="text-milk">{`}`}</p>
     </section>
   );
 };
